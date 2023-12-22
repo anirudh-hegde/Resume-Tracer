585cccd2d84fe4d52320f720b60f40f37dfd0712
Anianonymous <hegdeanirudh2003@gmail.com>
2023-12-22 12:02:12 +0530

    added files

---
+++ goat.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import PyPDF2
+
+# craete a empty string
+page_content = ""
+
+
+# function to extract the page contents of resume
+def display_context_file(upload_file):
+    global page_content
+    pdfReader = PyPDF2.PdfReader(upload_file)
+
+    for i in range(len(pdfReader.pages)):
+        pageObj = pdfReader.pages[i]
+        page_content = str(pageObj.extract_text())
+
+    return page_content
+
+
+def main():
+    st.title("Resume Search App")
+    upload_files = st.sidebar.file_uploader("upload your resume", type=['docx', 'pdf'])
+
+    # if file is uploaded process will satisfy
+    if upload_files:
+        st.success("Resume uploaded successfully")
+
+        # pass the uplaod_files
+        page_content = display_context_file(upload_files)
+        search_text = st.text_input("Enter the text which you want to search: ")
+
+        # for input/s
+        if search_text:
+            st.header("Searching results..")
+            for word in search_text.split(" "):
+                if word.lower() in page_content.lower():
+                    st.success(f"{word.capitalize()} present in the resume")
+
+                else:
+                    st.error(f"{word.capitalize()} isn't present in the resume")
+
+    else:
+        st.warning("Please upload the file")
+
+
+if __name__ == "__main__":
+    main()
