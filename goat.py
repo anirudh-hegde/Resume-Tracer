d053515c46b8f0d57fd10f9ce14a947cbc61b7f7
anirudh <hegdeanirudh2003@gmail.com>
2024-04-05 22:20:39 +0530

    code styled

--- goat.py
+++ goat.py
@@ -1,21 +1,23 @@
+'''Python goat file implements the streamlit app called as Resume-Tracer
+   which indentifies keywords required'''
+import cProfile
+from pypdf import PdfReader
 import streamlit as st
 
-from pypdf import PdfReader
-import cProfile
 # craete a empty string
-Page_Content = ""
+page_content = ""
 
 
 # function to extract the page contents of resume
 def display_context_file(upload_file):
     global page_content
-    Pdfreader = PdfReader(upload_file)
+    pdf_reader = PdfReader(upload_file)
 
     for i in range(len(pdfReader.pages)):
-        PageObj = pdfReader.pages[i]
-        Page_Content = str(PageObj.extract_text())
+        page_obj = pdf_reader.pages[i]
+        page_content = str(page_obj.extract_text())
 
-    return Page_Content
+    return page_content
 
 
 def main():
@@ -27,19 +29,19 @@ def main():
         st.success("Resume uploaded successfully")
 
         # pass the uplaod_files
-        Page_Content = display_context_file(upload_files)
-        Search_Text = st.text_input("Enter the text which you want to search: ")
+        page_content = display_context_file(upload_files)
+        search_text = st.text_input("Enter the text which you want to search: ")
 
         # for input/s
-        if Search_Text:
+        if search_text:
             st.header("Searching results..")
 
-            for word in Search_Text.split(","):
+            for word in search_text.split(","):
                 punctuations = [".", ".", "-", "*", "/"]
                 for p in punctuations:
                     word = word.lower().replace(p, "")
-                    Page_Content = Page_Content.lower().replace(p, "")
-                if word in Page_Content:
+                    page_content = page_content.lower().replace(p, "")
+                if word in page_content:
                     st.success(f"{word.capitalize()} present in the resume")
                 else:
                     st.error(f"{word.capitalize()} isn't present in the resume")
