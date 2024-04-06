251b49b6a6ef4edc9e501fa3141251184732d109
anirudh <hegdeanirudh2003@gmail.com>
2024-04-06 12:13:57 +0530

    code styled

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,8 +8,6 @@ def test_app_error():
     """tests any error occurs during testing the code of streamlit app"""
     test_dir=os.path.dirname(os.path.abspath(__file__))
     app_script_path=os.path.join(test_dir,"..","goat.py")
-
     assert os.path.isfile(app_script_path),f"File not found at {app_script_path}"
-
     at = AppTest.from_file(app_script_path).run()
     assert not at.exception
